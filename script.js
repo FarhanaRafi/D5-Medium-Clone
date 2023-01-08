497e351c46dd56754cecbbdf1767cdc0849e55bb
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-01-08 19:23:48 +0100

    M toggling

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ window.onscroll = function () {
     document.getElementById("button-header").classList.remove(buttonClassName);
   }
 };
+function mAnimation() {
+  let toggle = document.querySelectorAll("svg > g > g > g");
+  let random = Math.floor(Math.random() * toggle.length);
+  let randomly = Math.floor(Math.random() * 2);
+  toggle[random].style.opacity = randomly;
+}
+function toggler() {
+  setInterval(mAnimation, 50);
+}
+toggler();
